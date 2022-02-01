a37c7fe7f5113731255ea05f3d12ac09fbcd0924
Klebitz12890 <97742308+Klebitz12890@users.noreply.github.com>
2022-02-02 06:48:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6282260691984', '6281211006838', '6283128734012'] // Put your number here
+global.owner = ['6282260691984', '6281211006838', '6282124128334'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
