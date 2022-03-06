9ebb1ee98ddb252281b9d45f10e6cf042b8e4c2b
Klebitz12890 <97742308+Klebitz12890@users.noreply.github.com>
2022-03-06 11:58:16 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -30,27 +30,16 @@ let tags = {
 }
 const defaultMenu = {
   before: `
-╭─「 %me 」
-│ Hai, %name!
-│
-│ Tersisa *%limit Limit*
-│ Role *%role*
-│ Level *%level (%exp / %maxexp)* [%xp4levelup lagi untuk levelup]
-│ %totalexp XP in Total
-│ 
-│ Tanggal: *%week %weton, %date*
-│ Tanggal Islam: *%dateIslamic*
-│ Waktu: *%time*
-│
-│ Uptime: *%uptime (%muptime)*
-│ Database: %rtotalreg of %totalreg
-│ Github:
-│ %github
-╰────
+┌ 「 *MarsBOT-MD* 」
+│❑ Version: %version
+│❑ Library: Baileys-MD
+│❑ Mode: ${global.opts['self'] ? 'Self' : 'Publik'}
+│❑ Runtime: %uptime
+└─
 %readmore`.trimStart(),
-  header: '╭─「 %category 」',
-  body: '│ • %cmd %islimit %isPremium',
-  footer: '╰────\n',
+  header: '┌ *「 %category 」*',
+  body: '│ ≻ %cmd %islimit %isPremium',
+  footer: '└─\n',
   after: `
 *%npmname@^%version*
 ${'```%npmdesc```'}
