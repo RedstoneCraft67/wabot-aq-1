b7c10bc6928506f1f7fbe743d652f4ea26994e64
Klebitz12890 <97742308+Klebitz12890@users.noreply.github.com>
2022-03-06 15:56:59 +0700

    Create main-sewa.js

---
+++ plugins/main-sewa.js
@@ -0,0 +1,51 @@
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+let fs = require('fs')
+let handler = async (m) => {
+    let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+    else who = m.sender
+    let user = global.db.data.users[who]
+let anu = `LIST SEWA BOT\n
+10 Hari - 5k
+30 Hari - 20k\n
+Pulsa 
+Axis [083832492541] + 2k
+Indosat [085856508824] + 2k\n
+Non Pulsa
+Dana [083832492541]`
+     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+     templateMessage: {
+         hydratedTemplate: {
+           hydratedContentText: anu,
+           locationMessage: { 
+           jpegThumbnail: fs.readFileSync('./media/menu.jpg') }, 
+           hydratedFooterText: wm,
+           hydratedButtons: [{
+             urlButton: {
+               displayText: 'Saweria',
+               url: 'https://saweria.co/botrhnsh',
+             }
+
+           },
+               {
+             quickReplyButton: {
+               displayText: 'Owner',
+               id: '.owner',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
+}
+handler.help = ['sewabot']
+handler.tags = ['info']
+handler.command = /^(sewabot|sewa)$/i
+
+module.exports = handler
